347756c0f6da66125c1df0d446d2cd4e26df7881
JustADataConstruct <yarukatsaros@gmail.com>
2021-06-14 16:22:52 +0200

    Fixed lack of docs on last kata

--- 6kyu/counting_duplicates.py
+++ 6kyu/counting_duplicates.py
@@ -1,3 +1,9 @@
+"""
+6kyu "Counting duplicates" kata
+https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+14-06-21
+"""
 import codewars_test as test
 
 from collections import Counter
